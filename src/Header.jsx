703c332a55d57cca5ef347125b9dd40dbb08878e
Ellie-Lissner <eswalker0723@gmail.com>
2021-06-29 15:53:04 -0500

    header started

---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+export default function Header() {
+    return(
+        <nav>
+            <ul className="navList">
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">What We Do</a>
+                </li>
+                <li>
+                    <a href="#">Resources</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
